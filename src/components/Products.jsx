76822c694ea46b33202d65131ba8fae04be53be8
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-11-27 15:19:56 +0600

    delete data from database

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,31 @@
 import { useLoaderData } from "react-router-dom";
 
 const Products = () => {
-    const products = useLoaderData()
-    return (
-        <div>
-            <h1>Products:{products.length}</h1>
-        </div>
-    );
+  const products = useLoaderData();
+  const handleDelete = (id) => {
+    console.log(id);
+    fetch(`http://localhost:7000/products/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("successfully deleted");
+        }
+      });
+  };
+  return (
+    <div>
+      <h1>Products:{products.length}</h1>
+      {products.map((product) => (
+        <p key={product._id}>
+          {product.name}{" "}
+          <button onClick={() => handleDelete(product._id)}>x</button>
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
